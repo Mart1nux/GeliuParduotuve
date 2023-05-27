70d84c2852d23d4c0a594ddd8a89d62aa85154c9
AndrejJazevic <76114060+AndrejJazevic@users.noreply.github.com>
2023-05-27 22:29:34 +0300

    Some scope fix

--- src/main/java/com/itizwhatitiz/geliuparduotuve/rest/LoginController.java
+++ src/main/java/com/itizwhatitiz/geliuparduotuve/rest/LoginController.java
@@ -13,7 +13,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-@ApplicationScoped
+@RequestScoped
 @Path("/login")
 @Logger
 public class LoginController {
