38063c42c89f3048ba5766c4123a877e40d89cd8
geigosam <56157793+geigosam@users.noreply.github.com>
2023-05-27 20:36:28 +0300

    Updated logging

--- src/main/java/com/itizwhatitiz/geliuparduotuve/logger/LoggerImpl.java
+++ src/main/java/com/itizwhatitiz/geliuparduotuve/logger/LoggerImpl.java
@@ -17,7 +17,7 @@ import java.util.concurrent.CompletableFuture;
 public class LoggerImpl {
     @Inject
     CustomerDao customerDao;
-    String filename = "/home/nemo/mif/6/tp/GeliuParduotuve/src/main/java/com/itizwhatitiz/geliuparduotuve/logger/log.txt";
+    String filename = "../log.txt";
     @AroundInvoke
     public Object log(InvocationContext ctx) throws Exception {
         CompletableFuture.runAsync(() -> {
